625dbf2007651ccff40b9e0dcc11583997e9f6f2
Diogo Gomes <dgomes@av.it.pt>
2017-01-22 19:42:46 +0000

    Initial Commit
    Very Simple sketch

---
+++ home_ardbox.ino
@@ -0,0 +1,55 @@
+#include <relaybox.h>
+
+void callback(uint8_t relay_number, bool value) {
+  DEBUG_PRINTLN(relay_number); Serial.print(","); Serial.println(value?"true":"false");
+  Serial1.print(relay_number); Serial1.print(","); Serial1.println(value?"true":"false");
+}
+
+RelayBox ardbox(_20IO);
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(2400);
+#ifdef DEBUG
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }  
+  Serial.println(F("ARDBOX HOMIE v1"));
+#endif
+  Serial1.println("Hello from ARDBox");
+  Serial1.println(F("Usage: #relay,#period_milli\n"));
+  ardbox.setup(callback);
+}
+
+void loop() {
+  ardbox.loop();
+  String buf = "";
+  int relay_number = 0;
+  int period = 0;
+  size_t len = 0;
+
+  while (Serial1.available()) {
+    char in = Serial1.read();
+    len++;
+
+    buf += in;
+
+    if(in == ',') {
+      relay_number = atoi(buf.c_str());
+      buf = "";
+    }
+    if(in == '\n') {
+      period = atoi(buf.c_str());
+      buf = "";
+    }   
+    if(len>8)
+      break;
+    delay(20);
+  }
+  
+  if (relay_number > 0 && period > 0) {
+    ardbox.switchRelay(relay_number, period);
+  }
+
+
+}
